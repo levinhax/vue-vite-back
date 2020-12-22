4572aaf0b0e69988990dd17024f4e0331c7e0848
levinhax <levin1229@163.com>
2020-12-22 20:47:42 +0800

    feat: 修改为.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { defineComponent } from 'vue'
+import Logo from './assets/logo.png'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return () => (
+      <>
+        <img alt="Vue logo" src={Logo} />
+        <h2>Vue 后台管理系统</h2>
+      </>
+    )
+  },
+})
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    return () => (
+      <>
+        <h1>Home</h1>
+      </>
+    )
+  },
+})

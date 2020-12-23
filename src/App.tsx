96da469e44c6d091f5cbd0d81283b8ed3e0d8dcc
levinhax <levin1229@163.com>
2020-12-23 10:56:09 +0800

    feat: Antd .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { defineComponent } from 'vue'
 import Logo from './assets/logo.png'
+import { Button } from 'ant-design-vue'
 
 export default defineComponent({
   name: 'App',
+  components: {},
   setup() {
     return () => (
       <>
         <img alt="Vue logo" src={Logo} />
         <h2>Vue 后台管理系统</h2>
-        <a-button type="primary">按钮</a-button>
+
+        <Button type="primary">按钮</Button>
       </>
     )
   },
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './App'
 import './styles/index.scss'
 // import Antd from 'ant-design-vue'
-// import 'ant-design-vue/dist/antd.css'
-import { setupAntd } from './setup/ant-design-vue/index'
+import 'ant-design-vue/dist/antd.min.css'
+// import { setupAntd } from './setup/ant-design-vue/index'
 
 const app = createApp(App)
 
 // app.use(Antd)
-setupAntd(app)
+// setupAntd(app)
 
 app.mount('#app')
